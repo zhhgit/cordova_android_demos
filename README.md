3b26d45f3324445b4193403dbc03e50630870e71
zhhgit <zhh900601@sina.com>
2017-09-12 19:39:10 +0800

    revise demo1

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # cordova_cli_android_demo
 cordova_cli_android_demo
 
-cd demo1/MyApp
-
-npm install
+    cd demo1/MyApp
+    npm install
--- demo1/MyApp/platforms/android/assets/www/js/index.js
+++ demo1/MyApp/platforms/android/assets/www/js/index.js
@@ -1,46 +1,85 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+// 这个状态代表了应用程序的状态并且会在onResume()和onPause()中保存和恢复
+var appState = {
+    takingPicture: true,
+    imageUri: ""
 };
+
+var APP_STORAGE_KEY = "exampleAppState";
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        // 这里我们注册我们关心的生命周期事件回调
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('pause', this.onPause, false);
+        document.addEventListener('resume', this.onResume, false);
+    },
+    onDeviceReady: function() {
+        document.getElementById("take-picture-button").addEventListener("click", function() {
+            //由于camera插件方法启动了一个外部活动
+            //这里有一次机会我们的应用程序被kill掉在回调被成功或者失败调用之前
+            // 在onPause()和onResume()那里我们保存和恢复状态，来处理这个事情
+            appState.takingPicture = true;
+
+            navigator.camera.getPicture(cameraSuccessCallback, cameraFailureCallback,
+                {
+                    sourceType: Camera.PictureSourceType.CAMERA,
+                    destinationType: Camera.DestinationType.FILE_URI,
+                    targetWidth: 250,
+                    targetHeight: 250
+                }
+            );
+        });
+    },
+    onPause: function() {
+        // 这里我们检测我们是否在获取图片，如果在，我们希望保存我们的状态以便onResume()
+        // 恢复的时候使用，如果我们获得了图片URI我们也要存储
+        if(appState.takingPicture || appState.imageUri) {
+            window.localStorage.setItem(APP_STORAGE_KEY, JSON.stringify(appState));
+        }
+    },
+    onResume: function(event) {
+        // 这里我们检差存储的状态，如果需要恢复他。由你跟踪任何添加的插件结果的来源
+        //  (也就是说你代码的哪一步被调用)，还有什么参数提供给插件如果相关
+        var storedState = window.localStorage.getItem(APP_STORAGE_KEY);
+
+        if(storedState) {
+            appState = JSON.parse(storedState);
+        }
+
+        // 检查如果我们需要恢复我们的图片
+        if(!appState.takingPicture && appState.imageUri) {
+            document.getElementById("get-picture-result").src = appState.imageUri;
+        }
+        // 现在我们可以检测如果插件结果在事件对象里面
+        // 这里需要cordova-android 5.1.0+
+        else if(appState.takingPicture && event.pendingResult) {
+            // 检测插件调用是否成功并调用相应的回调。对于camera插件，"OK"
+            //意味着成功其他意味着错误
+            if(event.pendingResult.pluginStatus === "OK") {
+                // camera放置同样的结果在resume对象，因为成功回调传递给了getPicture(),
+                // 因此我们可以传递同样的回调，返回一些其他东西。查询文档，了解怎么解释你使用
+                // 插件的结果字段
+                cameraSuccessCallback(event.pendingResult.result);
+            } else {
+                cameraFailureCallback(event.pendingResult.result);
+            }
+        }
+    }
+}
+
+// 这里是回调我们传入getPicture()
+function cameraSuccessCallback(imageUri) {
+    appState.takingPicture = false;
+    appState.imageUri = imageUri;
+    document.getElementById("get-picture-result").src = imageUri;
+}
+
+function cameraFailureCallback(error) {
+    appState.takingPicture = false;
+    console.log(error);
+}
 
 app.initialize();
